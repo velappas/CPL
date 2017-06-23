07130a3db2b9c4da62a926eba4ba700a01b0c63a
velappas <vlappas1@gmail.com>
2017-06-23 15:42:05 -0600

    June 23

--- app/src/main/java/com/example/victorialappas/cpl/HRSearchFragment.java
+++ app/src/main/java/com/example/victorialappas/cpl/HRSearchFragment.java
@@ -6,16 +6,33 @@ package com.example.victorialappas.cpl;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import java.sql.*;
+import java.lang.*;
+
+
 
 public class HRSearchFragment extends Fragment {
+    //this needs to be edited
+     static String DB_IP = "jdbc:mysql://104.196.251.57/homebound_mySQL";
+     static String USER = "root";
+     static String PASS = "slkG8qUx";
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //Class.forName("com.mysql.jdbc.Driver").newInstance();
         return inflater.inflate(R.layout.hrsearch_fragment, container, false);
     }
+
+    public void launchSearch(){
+
+
+    }
+
+
+
+
+
 }
